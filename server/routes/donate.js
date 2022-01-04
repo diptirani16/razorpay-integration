ce39643fabb34a20fccd170c80757703037cd661
Dipti Rani <gdipti642@gmail.com>
2022-01-04 17:48:20 +0530

    add client part

--- server/routes/donate.js
+++ server/routes/donate.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const crpto = require('crypto');
 const express = require('express');
 const app = express();
 
@@ -6,21 +7,41 @@ const Razorpay = require('razorpay');
 
 app.post('/donate', async (req, res) => {
     try {
-        const razorpayInstance = new Razorpay({
+        const instance = new Razorpay({
             key_id: process.env.REACT_APP_KEY_ID,
             key_secret: process.env.REACT_APP_KEY_SECRET
         })
 
         const options = {
             amount: 500.00,
-            currency: 'INR',
-            // receipt: 'receipt_order_74394'
+            currency: 'INR'
         }
 
-        const order = await razorpayInstance.orders.create(options);
+        const order = await instance.orders.create(options);
         if(!order) return res.status(500).send('something went wrong!')
         res.json(order);
 
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+app.post('/success', (req, res) => {
+    try {
+        const { orderCreationId, razorpayPaymentId, razorpayOrderId, razorpaySignature } = req.body;
+        const shasum = crypto.createHmac("sha256", "w2lBtgmeuDUfnJVp43UpcaiT");
+        shasum.update(`${orderCreationId}|${razorpayPaymentId}`);
+        const digest = shasum.digest("hex");
+
+        if (digest !== razorpaySignature)
+            return res.status(400).json({ msg: "Transaction not legit!" });
+
+        res.json({
+            msg: "success",
+            orderId: razorpayOrderId,
+            paymentId: razorpayPaymentId,
+        })
+        
     } catch (error) {
         res.status(500).send(error);
     }
--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const donateController = require('./routes/donate');
+const cors = require('cors');
+
 const app = express();
 
+app.use(cors());
 app.use(express.json({extended: false}));
 
-app.use("/donate", donateController);
+app.use("/pay", donateController);
 
 app.listen(8080, () => console.log('server running on port 8080'));
